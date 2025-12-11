0272d167c3fa971e57802b3bf6544a3d45aabfe7
Samuel Fredric Berg <samuelberg.private@gmail.com>
2025-12-11 07:58:30 +0100

    chore: Started working on a2

---
+++ a2/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"a2/src"
+	"fmt"
+)
+
+func main() {
+	fmt.Println("=== Running problem 1 ===")
+	src.P1()
+
+	fmt.Println("\n=== Running problem 2 ===")
+	src.P2()
+
+	fmt.Println("\n=== Running problem 3 ===")
+	src.P3()
+}
---
+++ a2/src/p1.go
@@ -0,0 +1,5 @@
+package src
+
+func P1() {
+
+}
